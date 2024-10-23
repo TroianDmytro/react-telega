28b7b1b81f29d72be28e0a89ce97c47ffe0d8bbb
TroianDmytro <troyan.t64@gmail.com>
2024-10-23 21:58:28 +0300

    #

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
+import Table from 'react-bootstrap/esm/Table'
+import axios from 'axios';
+import { format } from 'date-fns';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface User {
+  recordId: string; // GUID в C# представлений як string в TypeScript
+  userId: number; // long в C# представлений як number в TypeScript
+  firstName: string; // string в C# представлений як string в TypeScript
+  lastName: string; // string в C# представлений як string в TypeScript
+  telegramName?: string; // ? означає, що це поле є необов'язковим
+  createDate: Date; // DateTime в C# представлений як Date в TypeScript
 }
 
-export default App;
+function App() {
+  const [listUsers,setListUsers] = useState<User[]|null>(null);
+  useEffect(()=>{
+    const getAllUsers=async()=>{
+      try{
+        const result = await axios.get("http://localhost:5265/api");
+        setListUsers(result.data);
+      }
+      catch(err){
+        console.log(err);
+      }
+    }
+    getAllUsers();
+  },[setListUsers])
+
+  useEffect(()=>{
+    console.log(listUsers);
+  },[listUsers]);
+
+  function dateString(date: Date) {
+    const currentDate: Date = date;
+    const formattedDate: string = format(currentDate, 'dd/MM/yyyy HH:mm:ss');
+    return formattedDate;
+  }
+
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>User Id</th>
+            <th>Telegram Name</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Date and time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUsers?.map((item,index)=>{
+            return(
+            <tr key={index}>
+              <td>{index+1}</td>
+              <td>{item.userId}</td>
+              <td>{item.telegramName}</td>
+              <td>{item.firstName}</td>
+              <td>{item.lastName}</td>
+              <td>{dateString(item.createDate)}</td>
+            </tr>)
+          })}
+        </tbody>
+      </Table>
+    </>
+  )
+}
+
+export default App
